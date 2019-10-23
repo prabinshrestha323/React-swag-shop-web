1e77196f59bcf15c11f04d7b301fb9701c19b04d
prabinshrestha323 <prabinshrestha323@gmail.com>
2019-10-23 19:36:59 +0545

    my fist react application

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HttpService from "../services/http-service";
+import { tsConstructorType } from "@babel/types";
+const http = new HttpService();
+var props;
+var Super;
 
 function App() {
+  constructor(props);
+  {
+    Super(props);
+    http.getProduct();
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to the Swag shop</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
